56d84494375a91dac70d71006aef7d08ec19547b
Adam Day <advation@gmail.com>
2019-05-04 21:49:57 -0600

    Added a print amount loop

--- main.py
+++ main.py
@@ -291,9 +291,9 @@ def print_amount_loop():
 
         image = pygame.image.load('/home/pi/PiPhotobooth/images/%s.jpg' % captured_image)
         screen.blit(image, (80, 15))
-        pygame.draw.rect(screen, BLUE, [20, ((height / 2) + 50), 150, 150])
-        pygame.draw.rect(screen, BLUE, [180, ((height / 2) + 50), 150, 150])
-        pygame.draw.rect(screen, BLUE, [360, ((height / 2) + 50), 150, 150])
+        pygame.draw.rect(screen, BLUE, [185, ((height / 2) + 50), 150, 150])
+        pygame.draw.rect(screen, BLUE, [245, ((height / 2) + 50), 150, 150])
+        pygame.draw.rect(screen, BLUE, [705, ((height / 2) + 50), 150, 150])
         pygame.display.update()
 
 
